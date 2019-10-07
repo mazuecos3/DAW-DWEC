54fc02ca6f9f6a1a9c7870a791652edb74cc830d
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-10-07 02:51:05 +0200

    finalizando ej dom

--- JS/EjercicioDom/dom.js
+++ JS/EjercicioDom/dom.js
@@ -1,32 +1,43 @@
 // Redondear el cuadrado y añadir tiempo de transicion de 0.5s
 function HacerCirculo(Redondear) {
 
-
     Redondear.style.borderRadius = "50%";
+    Redondear.style.transitionDuration = "1s";
 
-    Redondear.style.transitionDuration = "0.5s";
 }
 
 // Quitar el redondeo de el circulo y volver al cuadrado en 0.5s
 function QuitarCirculo(cuadrdoADesredondear) {
 
-
     cuadrdoADesredondear.style.borderRadius = "0%";
+    cuadrdoADesredondear.style.transitionDuration = "1s";
 
 
-    cuadrdoADesredondear.style.transitionDuration = "0.5s";
 }
 
-// Eliminar cuadrado seleccionado
-function eliminarCuadrado(Cuadrado) {
+
+// Boton de eliminar
+
+function eliminarCuadrado(boton) {
+
+    boton.parentNode.remove();
+
+}
+
+// Cambiar posicion sombra
+
+function cambiarSombra(sombra) {
+
+    sombra.style.boxShadow = "inset -16px  -20px 15px  -6px rgba(0, 0, 0, 0.75)";
 
 
-    var elementoCuadrado = document.getElementsByClassName("Cuadrado");
+}
 
 
-    var padreCuadrados = document.getElementsByClassName("general");
+// Volver a la sombra
+function quitarSombra(sombra) {
 
+    sombra.style.boxShadow = " 0px  0px 0px  0px rgba(0, 0, 0, 0.75)";
 
-    padreCuadrados.removeChild(elementoCuadrado[0]);
 
 }
