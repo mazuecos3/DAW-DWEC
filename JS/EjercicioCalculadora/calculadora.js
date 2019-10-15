0742707ce2c8f019070f0d5666a8caf7f5f73d68
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-10-15 04:02:11 +0200

    Retocar bugs y añadir %

--- JS/EjercicioCalculadora/calculadora.js
+++ JS/EjercicioCalculadora/calculadora.js
@@ -1,25 +1,188 @@
 var pantalla;
 
-window.onload = function(){
+
+
+var Comprobaroperacion;
+
+
+
+
+
+
+window.onload = function() {
 
     pantalla = document.getElementById("pantalla");
     var botones = document.getElementsByClassName("boton");
 
-    for(let i = 0; i < botones.length; i++) {
+    var resultadoEval;
 
-        botones[i].addEventListener("click", mostrar);
-        botones[0].addEventListener("click", borrar);
+    for (let i = 0; i < botones.length; i++) {
+
+        botones[i].addEventListener("click", ComprobarOperacion);
+        botones[i].addEventListener("mousedown", sombraInterior);
+        botones[i].addEventListener("mouseup", quitarSombra);
+
+
+
+    }
+
+
+}
+
+
+function sombraInterior() {
+    this.classList.add("sombraInterior");
+}
+
+function quitarSombra() {
+
+    this.classList.remove("sombraInterior");
+
+}
+
+
+function ComprobarOperacion() {
+
+    var escribir = this.innerText;
+
+
+
+    if (isNaN(escribir)) {
+        Operacion(escribir);
+    } else {
+        mostrar(escribir);
+    }
+
+
+
+}
+
+function mostrar(escribir) {
+
+    // Averiguar coma
+    /*
+        if (pantalla.value == 0) {
+            pantalla.value = '0.';
+        } else if (pantalla.value.indexOf('.') == -1) {
+            pantalla.value += '.';
+        }
+
+    */
+
+    if (pantalla.value != 0) {
+        pantalla.value += escribir;
+    } else {
+        pantalla.value = escribir;
+    }
+
+    if (pantalla.value == "") {
+
+        pantalla.value = "0"
+
+    }
+
+    this.resultadoEval = pantalla.value;
+
+}
+
+function Operacion(escribir) {
+
+    let operacion = "";
+
+    switch (escribir) {
+        case "C":
+            borrar();
+
+            break;
+
+        case "%":
+            porcentaje();
+            operacion = "%";
+            break;
+
+        case "«":
+            borrarFlecha();
+            break;
+
+        case "/":
+
+            operacion = "/";
+            break;
+
+        case "x":
+
+            operacion = "*";
+            break;
+
+        case "-":
+
+            operacion = "-";
+            break;
+
+        case "+":
+
+            operacion = "+";
+            break;
+
+        case "()":
+
+            operacion = "()";
+            parentesis();
+            break;
+
+        case "=":
+
+            calcular();
+            break;
+
+    }
+
+
+    if (!isNaN(pantalla.value.substring(pantalla.value.length - 1, pantalla.value.length))) {
+
+        mostrar(operacion);
+    }
+
+}
+
+
+function borrar() {
+
+    pantalla.value = "0";
+}
+
+
+
+function porcentaje() {
+
+
+}
+
+function borrarFlecha() {
+
+    pantalla.value = pantalla.value.substring(0, pantalla.value.length - 1);
+
+    if (pantalla.value == "") {
+
+        pantalla.value = "0"
+
     }
 }
- 
-function mostrar() {
-  
-    pantalla.value += this.innerText;
-    
+
+
+function calcular() {
+
+    let resultado = eval(this.resultadoEval);
+
+    pantalla.value = resultado;
+
 }
 
-function borrar(){
+function parentesis() {
 
-    
-    pantalla.value = "0";
+    if (!pantalla.value.includes("(")) {
+        pantalla.value = "(" + pantalla.value + ")";
+    }
+
+
 }
